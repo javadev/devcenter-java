ec3d58f1c01c9e2eefeba63e0888bc3d7984b538
javadev <javadev75@gmail.com>
2018-11-08 06:06:16 +0200

    Add response.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,13 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
+import com.github.underscore.lodash.U;
 
 public class HelloWorld extends HttpServlet {
 
@@ -10,11 +15,23 @@ public class HelloWorld extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         resp.setHeader("Content-Type", "text/javascript");
+        resp.setHeader("Cache-Control", "no-cache, must-revalidate");
         resp.setHeader("Server", "Microsoft-IIS/10.0");
         resp.setCharacterEncoding("UTF-8");
         final String callback = req.getParameter("callback");
         final String url = req.getParameter("url");
-        resp.getWriter().print("Hello from Java!\n" + " - callback=" + callback + " - url=" + url);
+        StringBuilder builder = new StringBuilder();
+        builder.append(callback).append("(\n");
+        final Map<String, Object> json = new LinkedHashMap<>();
+        json.put("server", "www.ddginc-usa.com");
+        json.put("remote_host", "95.132.175.241");
+        json.put("url", url);
+        final List<String> html = new ArrayList<>();
+        html.add(U.fetch(url).text());
+        json.put("html", html);
+        builder.append(U.toJson(json)).append("\n)");
+        resp.setHeader("Content-Length", "" + builder.length());
+        resp.getWriter().print(builder.toString());
     }
 
     public static void main(String[] args) throws Exception{
