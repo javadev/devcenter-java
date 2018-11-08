c8de72d6e0d531bcd7d2bd559cdc138715d9377f
javadev <javadev75@gmail.com>
2018-11-08 05:34:51 +0200

    Add encoding.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,9 +9,9 @@ public class HelloWorld extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.getWriter().print("Hello from Java!\n");
         resp.setHeader("Content-Type", "text/javascript");
         resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().print("Hello from Java!\n");
     }
 
     public static void main(String[] args) throws Exception{
